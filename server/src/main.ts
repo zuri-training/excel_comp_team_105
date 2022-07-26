4b936945bda4aa7b5bc6a671ac99a57d3acf25cc
Damilare Okubanjo <damilare.okubanjo@indicina.co>
2022-07-26 11:12:23 +0100

    base file structure setup

---
+++ server/src/main.ts
@@ -0,0 +1,33 @@
+import { NestFactory } from '@nestjs/core';
+import {
+  DocumentBuilder,
+  SwaggerCustomOptions,
+  SwaggerDocumentOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Excel Project')
+    .setDescription('REST API for Zuri Excel Project')
+    .setVersion('v1')
+    .addTag('excel')
+    .build();
+
+  const docs = SwaggerModule.createDocument(app, config);
+
+  const customOptions: SwaggerCustomOptions = {
+    customSiteTitle: 'Excel Project API Docs',
+  };
+
+  SwaggerModule.setup('api', app, docs, customOptions);
+
+  const options: SwaggerDocumentOptions = {
+    deepScanRoutes: true,
+  };
+
+  await app.listen(8000);
+}
+bootstrap();
