a9c077fd1324aee8c5a1abc26de13e27c1c35599
dwhyq <dwhyq@outlook.com>
2022-07-28 21:21:36 +0100

    styled the sign-up page

--- client/src/Components/Pages/sign-up page/SignUp.jsx
+++ client/src/Components/Pages/sign-up page/SignUp.jsx
@@ -8,38 +8,58 @@ import { FcGoogle } from "react-icons/fc";
 const SignUp = () => {
   return (
     <>
-      <form>
-        <div>
-          <h1>Welcome</h1>
-          <p>Create Account</p>
-        </div>
-        <div>
-          <Input name="Full Name" type="text" placeholder="Shearly Wilson" />
-          <Input
-            name="Email Address"
-            type="password"
-            placeholder="shearltwilson@address.com"
-          />
-          <Input name="Password" type="password" placeholder="*********" />
-          <div className="checkbox-container">
-            <input type="checkbox" name="Remember-me" />
-            <label className="remember-me" htmlFor="Remember-me">
-              Yes I have read and agreed to the Terms of Service and Privacy
-              Policy.
+    <div className="loginContainer">
+      <h1 className="logo">XelComp</h1>
+        <form className="form">
+          <div className="welcome">
+            <h2>Welcome</h2>
+            <p>Create Account</p>
+          </div>
+          <div>
+            
+            <label>
+              Full Name
+              <div className="inputbox">
+                <input type="text" placeholder="Shearly Wilson"/>
+              </div>
             </label>
+
+            <label>
+              Email Address
+              <div className="inputbox">
+                <input type="email" placeholder="shearltwilson@address.com"/>
+              </div>
+            </label>
+
+            <label className="label">
+              Password
+              <div className="inputbox">
+                <input type="Password" placeholder="*********"/>
+              </div>
+            </label>
+            
+            <label className="checkbox">
+              <input type="checkbox" id="TnC" name="TnC"/>
+              <span>Yes I have read and agreed to the Terms</span><br/>
+              <p className="ppolicy">of Service and Privacy
+                Policy.</p>
+            </label>
+            
+
           </div>
-        </div>
-        <div>
-          <Button>Create Account</Button>
-          <div>
-            Already have an account? <span>Log in</span>
+
+          <div className="crtAccount">
+            <Button>Create Account</Button>
+            <div className="HaveAnAcc">
+              <span>Already have an account?</span> <a href="LoginPage.jsx">Log in</a>
+            </div>
+            <div className="sgnWitGogle">
+              <FcGoogle className="GoogleLogo"/>
+              <p>sign in with Google</p>
+            </div>
           </div>
-          <div>
-            <FcGoogle />
-            <p>sign in with Google</p>
-          </div>
-        </div>
-      </form>
+        </form>
+    </div>
     </>
   );
 };
