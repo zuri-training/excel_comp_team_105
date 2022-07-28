5de1ec2bbf29bc88b337fc827170cc9728301813
Master-Daniels <adebayooluwasegun011@gmail.com>
2022-07-28 14:09:27 +0100

    Touched up the login page

--- client/src/Components/Pages/login_page/LoginPage.jsx
+++ client/src/Components/Pages/login_page/LoginPage.jsx
@@ -1,48 +1,44 @@
 import React from "react";
-import Button from "../../Reuseable components/Button/Button";
-import Input from "../../Reuseable components/Input/Input";
 import "./login_page.css";
 
 const LoginPage = () => {
   return (
     <div id="body">
-
       <div className="loginContainer">
-        <h1>XelComp</h1>
-        
         <form>
           <h2>Login</h2>
 
           <label>
             Username
             <div className="inputbox">
-              <input type="username" placeholder="Shearly Wilson"/>
+              <input type="username" placeholder="Shearly Wilson" />
             </div>
           </label>
 
           <label>
             Password
             <div className="inputbox">
-              <input type="Password" placeholder="*********"/>
+              <input type="Password" placeholder="*********" />
             </div>
           </label>
 
           <label className="checkbox">
-            <input type="checkbox" id="Remember me" name="Remember me"/><span>Remember me</span>
+            <input type="checkbox" id="Remember me" name="Remember me" />
+            <span>Remember me</span>
           </label>
-        
+
           <div className="button">
             <button>Log In</button>
           </div>
 
           <div className="forgotReset">
-            <a>Forgot Password?</a> <a><span>Reset</span></a>
+            <a href="/forgot">Forgot Password?</a>
+            <a href="/forgot">
+              <span>Reset</span>
+            </a>
           </div>
-
         </form>
-        
       </div>
-      
     </div>
   );
 };
