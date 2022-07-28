1d55b31460e8e801c0b4697f20fe6bc9bf427aeb
Master-Daniels <adebayooluwasegun011@gmail.com>
2022-07-28 01:01:28 +0100

    Finished working on the navbar, started landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,10 @@
+import LandingPage from "./Components/Pages/landing-page/LandingPage";
 import "./App.css";
-import LoginPage from "./Components/Pages/login_page/LoginPage";
-import SignUp from "./Components/Pages/sign-up page/SignUp";
-import Navbar from "./Components/Static-components/Navbar/Navbar";
 
 function App() {
   return (
     <>
-      <Navbar />
+      <LandingPage />
     </>
   );
 }
