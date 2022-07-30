1dc1a4df88372a15170311c982dff1538dd07441
Master-Daniels <adebayooluwasegun011@gmail.com>
2022-07-30 04:38:48 +0100

    Little changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
-import LoginPage from "./Components/Pages/login_page/LoginPage";
-import SignUp from "./Components/Pages/sign-up page/SignUp";
-import Navbar from "./Components/Static-components/Navbar/Navbar";
-import LandingPage from "./Components/Pages/landing-page/LandingPage";
+// import LoginPage from "./Components/Pages/login_page/LoginPage";
+// import SignUp from "./Components/Pages/sign-up page/SignUp";
+// import Navbar from "./Components/Static-components/Navbar/Navbar";
+// import LandingPage from "./Components/Pages/landing-page/LandingPage";
 import DashboardHome from "./Components/Pages/dashboard_home/DashboardHome";
 
 
