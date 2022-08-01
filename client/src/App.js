d7502cb0c966334a0e43786bded7e4fd81aa89ef
Zinniie <ezinne.blessing@yahoo.com>
2022-08-01 20:54:28 +0100

    dashboard page

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,9 @@ import React from "react";
 import "./App.css";
 
 
-// import DashboardHome from "./Components/Pages/dashboard_home/DashboardHome";
-// import DashboardNpsf from "./Components/Pages/dashboard-new project_single file/DashboardNpsf";
-// import DashboardCompare from "./Components/Pages/dashboard_compare_single/DashboardCompare";
+import DashboardHome from "./Components/Pages/dashboard_home/DashboardHome";
+import DashboardNpsf from "./Components/Pages/dashboard-new project_single file/DashboardNpsf";
+import DashboardCompare from "./Components/Pages/dashboard_compare_single/DashboardCompare";
 import DashboardCompare2Files from "./Components/Pages/dashboard_compare2files/DashboardCompare2Files";
 
 
