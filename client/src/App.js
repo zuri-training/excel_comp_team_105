dffebc3f1844198bd383c0ef62840c632aa2ffa7
Master-Daniels <adebayooluwasegun011@gmail.com>
2022-08-01 07:08:13 +0100

    Cleaned up app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,8 @@
 import React from "react";
 import "./App.css";
-import ContactUs from "./Components/Pages/ContactUs/ContactUs";
-// import ErrorPage from "./Components/Pages/ErrorPage/ErrorPage";
-// import LoginPage from "./Components/Pages/login_page/LoginPage";
-// import LandingPage from "./Components/Pages/landing-page/LandingPage";
-//import SignUp from "./Components/Pages/sign-up page/SignUp";
 
 function App() {
-  return (
-    <>
-      <ContactUs />
-      {/* <LoginPage /> */}
-    </>
-  );
+  return <></>;
 }
 
 export default App;
