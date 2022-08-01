4201116ed0c844b1717016cb543c9445b98ad4fb
Master-Daniels <adebayooluwasegun011@gmail.com>
2022-08-01 08:37:08 +0100

    Installed firebase

--- client/src/Components/Pages/sign-up page/SignUp.jsx
+++ client/src/Components/Pages/sign-up page/SignUp.jsx
@@ -7,47 +7,85 @@ import "./sign-up.css";
 
 // Icon
 import { FcGoogle } from "react-icons/fc";
+import { SiFacebook } from "react-icons/si";
 
 // Firebase
 import { createUser } from "../../../Firebase/firebase.utils";
 
-const SignUp = () => {
-  return (
-    <>
-      <form>
-        <div>
-          <h1>Welcome</h1>
-          <p>Create Account</p>
-        </div>
-        <div>
-          <Input name="Full Name" type="text" placeholder="Shearly Wilson" />
-          <Input
-            name="Email Address"
-            type="password"
-            placeholder="shearltwilson@address.com"
-          />
-          <Input name="Password" type="password" placeholder="*********" />
-          <div className="checkbox-container">
-            <input type="checkbox" name="Remember-me" />
-            <label className="remember-me" htmlFor="Remember-me">
-              Yes I have read and agreed to the Terms of Service and Privacy
-              Policy.
-            </label>
-          </div>
-        </div>
-        <div>
-          <Button>Create Account</Button>
-          <div>
-            Already have an account? <span>Log in</span>
-          </div>
-          <div>
-            <FcGoogle />
-            <p>sign in with Google</p>
-          </div>
-        </div>
-      </form>
-    </>
-  );
-};
+class SignUp extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      name: "",
+      email: "",
+      password: "",
+    };
+  }
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+  handleSubmit = (event) => {
+    const { email, password } = this.state;
+    event.preventDefault();
+    // createUser(email, password);
+    console.log(email, password);
+  };
+  render() {
+    return (
+      <>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <h1>Welcome</h1>
+            <p>Create Account</p>
+          </div>
+          <div>
+            <Input
+              name="name"
+              type="text"
+              placeholder="Shearly Wilson"
+              handleChange={this.handleChange}
+            />
+            <Input
+              name="email"
+              type="password"
+              placeholder="shearltwilson@address.com"
+              handleChange={this.handleChange}
+            />
+            <Input
+              name="password"
+              type="password"
+              placeholder="*********"
+              handleChange={this.handleChange}
+            />
+            <div className="checkbox-container">
+              <input type="checkbox" name="Remember-me" />
+              <label className="remember-me" htmlFor="Remember-me">
+                Yes I have read and agreed to the Terms of Service and Privacy
+                Policy.
+              </label>
+            </div>
+          </div>
+          <div>
+            <Button>Create Account</Button>
+            <div>
+              Already have an account? <span>Log in</span>
+            </div>
+            <div>
+              <FcGoogle />
+              <p>Sign in with Google</p>
+            </div>
+            <div>
+              <SiFacebook />
+              <p>Sign in with Facebook</p>
+            </div>
+          </div>
+        </form>
+      </>
+    );
+  }
+}
 export default SignUp;
