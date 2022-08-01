9106e0a3838f02f13b859332e31cad5b78dfbbf5
Master-Daniels <adebayooluwasegun011@gmail.com>
2022-08-01 21:49:38 +0100

    finished working on the design for the sign up page

--- client/src/Components/Pages/sign-up page/SignUp.jsx
+++ client/src/Components/Pages/sign-up page/SignUp.jsx
@@ -5,19 +5,20 @@ import Button from "../../Reuseable components/Button/Button";
 
 import "./sign-up.css";
 
-// Icon
-import { FcGoogle } from "react-icons/fc";
+// Icons
+import { AiFillGoogleCircle } from "react-icons/ai";
+import { SiFacebook } from "react-icons/si";
 
 const SignUp = () => {
   return (
     <>
-      <div className="container_login" >
+      <div className="container_login">
         <form>
-          <div className="welcome-text" >
+          <div className="welcome-text">
             <h1>Welcome</h1>
             <p>Create Account</p>
           </div>
-          <div className="inputs" >
+          <div className="inputs">
             <Input name="Full Name" type="text" placeholder="Shearly Wilson" />
             <Input
               name="Email"
@@ -26,27 +27,22 @@ const SignUp = () => {
             />
             <Input name="Password" type="password" placeholder="************" />
           </div>
-          <div>
-            <label className="checkbox" htmlFor="Terms">
-              <input type="checkbox" id="Terms" name="Terms" />
-              <span>
-                Yes I have read and agreed to the terms service and privacy
-                policy
-              </span>
-            </label>
+          <div className="checkbox">
+            <input type="checkbox" id="Terms" name="Terms" />
+            <label htmlFor="Terms">Terms of Service</label>
           </div>
-          <div>
-            <Button>Create Account</Button>
-            <div>
-              <span>
-                Already have an account?
-                <a href="/login">Log in</a>
-              </span>
-            </div>
-            <div>
-              <FcGoogle/>
-              <p>Sign Up With Google</p>
-            </div>
+          <div className="button">
+            <Button>Sign Up</Button>
+          </div>
+          <div className="account-login">
+            <span>
+              Already have an account?
+              <a href="/login">Log in</a>
+            </span>
+          </div>
+          <div className="socials">
+            <AiFillGoogleCircle className="google" />
+            <SiFacebook />
           </div>
         </form>
       </div>
