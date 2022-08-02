5985fe3dbc62b6d7e566fce0158b9a6fa52b53f7
dwhyq <dwhyq@outlook.com>
2022-08-02 01:20:07 +0100

    Done with the Dashboard saved file first webpage

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 
 import React from "react";
-import LandingPage from "./Components/Pages/landing-page/LandingPage";
+// import LandingPage from "./Components/Pages/landing-page/LandingPage";
 import "./App.css";
-
+import DashBoardSf from "./Components/Pages/DashBoardSf/DashBoardSf"
 // import LoginPage from "./Components/Pages/login_page/LoginPage";
-import SignUp from "./Components/Pages/sign-up page/SignUp";
+// import SignUp from "./Components/Pages/sign-up page/SignUp";
 // import Navbar from "./Components/Static-components/Navbar/Navbar";
-import Footer from "./Components/Pages/Footer/footer";
+// import Footer from "./Components/Pages/Footer/footer";
+// import LoginPage from "./Components/Pages/login_page/LoginPage";
 
 
 
@@ -17,15 +18,9 @@ function App() {
   return (
     <>
 
-
-    <SignUp/>
-
-
-
-
-
-
-
+    {/* <SignUp/> */}
+    {/* <LoginPage/> */}
+    <DashBoardSf/>
 
     </>
   );
