161d922cbadcb7fbbd031a60c5f6105fd671c2c0
Master-Daniels <adebayooluwasegun011@gmail.com>
2022-08-09 22:35:13 +0100

    Centered landing page text

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
-//import LandingPage from "./Components/Pages/landing-page/LandingPage";
 import "./App.css";
+
+//import LandingPage from "./Components/Pages/landing-page/LandingPage";
 //import CorrectError_tf from "./Components/Pages/CorrectError_tf/CorrectError_tf";
 //import SignUp from "./Components/Pages/sign-up page/SignUp";
 //import Contact_Us from "./Components/Pages/Contact_Us/contact";
 //import CorrectError_sf from "./Components/Pages/CorrectError_sf/CorrectError_sf";
-import Differences_2files from "./Components/Pages/Differences_2Files/differences_2files";
+//import Differences_2files from "./Components/Pages/Differences_2Files/differences_2files";
 //import Duplicates_2files from "./Components/Pages/Duplicates_2Files/duplicates_2files";
-
-import "./App.css";
 
 // Custom Components --Used
 import SignUp from "./Components/Pages/sign-up page/SignUp";
@@ -39,7 +38,7 @@ function App() {
     const unsub = monitorAuthState(setCurrentUser);
     return () => unsub();
   }, []);
-  
+
   return (
     <div>
       <Routes>
